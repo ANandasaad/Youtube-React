5012677f7470ff02451bf4cfa4cd5ea1a5f1f132
ANandasaad <anandkushwaha70@gmail.com>
2023-03-25 00:01:28 +0530

    search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,87 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { toggleMenu } from '../utils/appSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
 
 const Header = () => {
-  const dispatch =useDispatch();
-  const toggleMenuHandler=()=>{
-      dispatch(toggleMenu());
-  }
+  const [searchQuery, setQuery] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
+  const [showSuggestion,setShowSuggestion]=useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    // console.log(searchQuery);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    console.log("API CAll" + searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+
+    setSuggestion(json[1]);
+
+    console.log(json[1]);
+  };
+
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
-      <img  onClick={()=>toggleMenuHandler()} className="h-10  hover:cursor-pointer" alt="ham" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="/>
-     <a href='/'><img className="h-10 mx-2 hover:cursor-pointer" alt="youtube" src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png"/></a>  
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-10  hover:cursor-pointer"
+          alt="ham"
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+        />
+        <a href="/">
+          <img
+            className="h-10 mx-2 hover:cursor-pointer"
+            alt="youtube"
+            src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png"
+          />
+        </a>
       </div>
       <div className="col-span-10 px-10">
-        <input type="text" className=" w-1/2  p-2 border border-gray-400 rounded-l-full"/>
-        <button className=" border border-gray-400 p-2 rounded-r-full">Search</button>
+        <div>
+          <input
+            type="text"
+            className=" w-1/2  px-5 border border-gray-400 p-2 rounded-l-full"
+            value={searchQuery}
+            onChange={(e) => setQuery(e.target.value)}
+            onFocus={()=>setShowSuggestion(true)}
+            onBlur={()=>setShowSuggestion(false)}
+          />
+          <button className=" border border-gray-400 p-2 rounded-r-full">
+            Search
+          </button>
+        </div>
+       { showSuggestion && <div className="fixed bg-white py-2 px-2 w-[31rem] shadow-lg rounded-lg border border-gray-100">
+          <ul>
+            {suggestion.map((s) => (
+              <li key={s} className="px-2 py-2 hover:bg-gray-100">
+                &#128269; {s}
+              </li>
+            ))}
+          </ul>
+        </div>}
       </div>
 
       <div className="col-span-1 ">
-        <img className="w-11 hover:cursor-pointer" alt="profile" src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"/>
+        <img
+          className="w-11 hover:cursor-pointer"
+          alt="profile"
+          src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
+        />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,4 @@ const YOUTUBE_API_KEY="AIzaSyCNUxWnmfTQkzgIKgW8KxsoFI72jCTEJfA";
 export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+YOUTUBE_API_KEY;
 
 
+export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
