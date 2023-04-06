f7c7b38c8d6e45ac846edfc2b7afe74fc48d864c
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-04-06 14:36:25 +0530

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ import RelatedVideo from "../components/RelatedVideo";
 export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+YOUTUBE_API_KEY;
  export const RELATED_VIDEO="https://youtube.googleapis.com/youtube/v3"
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+export const YOUTUBE_SEARCH_API="https://corsanywhere.herokuapp.com/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 
 export const LIVE_CHAT_COUNT=40;
